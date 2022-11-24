441dc89c662ffb210ea5cdb0330e74aa5c6fa26b
edwardkinglee <edward.lee3@telus.com>
2022-11-24 17:18:16 -0500

    tested code and had mentor review

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,24 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const search = args[0];
+
 const api = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request(api + search, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  }
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('No results found.');
-    return 0;
-  }
-  //console.log(data);
-  //console.log(typeof data);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function (breedName, callback) {
+
+  request(api + breedName, (error, response, body) => {
+   
+    if (error) {
+      return callback(error,null);
+    }
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback('No results found', null);
+    }
+
+    breedName = data[0].description;
+    callback(error, breedName);
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
